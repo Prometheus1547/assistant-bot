25908cd266cca21919c1af73a928841ceb4224ac
VladislavBut <magic.needle.md@gmail.com>
2022-07-22 23:28:27 +0300

    В main добавил кнопку Status В order добавил классы состояний

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from config import TOKEN
 from markups import ikb_menu
-from order import Action, Feel
+from order import Action, Feel, Status
 
 bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-#___________________________________КНОПКА Action________________________________________
+
+# ___________________________________BUTTON ACTION________________________________________
 async def start(message: types.Message):
     await message.answer("Please, choose something", reply_markup=ikb_menu)
+
 
 @dp.callback_query_handler(lambda c: c.data == 'action')
 async def handle_action(call: types.CallbackQuery):
@@ -20,40 +22,76 @@ async def handle_action(call: types.CallbackQuery):
     await bot.send_message(call.from_user.id, 'Please,write the name of the action')
     await Action.state_action.set()
 
-async def name_action(message: types.Message, state: FSMContext): #Сохранили название Action в список
+
+async def name_action(message: types.Message, state: FSMContext):  #Сохранили название Action в список
     await state.update_data(action_name=message.text.lower())
     user_data = await state.get_data()
     loop = asyncio.get_event_loop()
-    loop.run_in_executor(None, requests.post, "https://cernev-assistant-api.herokuapp.com/api/v1/action", {'label': user_data['action_name']})
+    loop.run_in_executor(None, requests.post, "https://cernev-assistant-api.herokuapp.com/api/v1/action",
+                         {'label': user_data['action_name']})
     await message.answer(f"Thank you, action: {user_data['action_name']} is recorded")
     await state.finish()
 
 
-#___________________________________КНОПКА Feel________________________________________
+# ___________________________________BUTTON FEEL________________________________________
 @dp.callback_query_handler(lambda c: c.data == 'feel')
 async def handle_action(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     await bot.send_message(call.from_user.id, 'Please,write the name of the feel')
     await Feel.wait1.set()
 
+
 async def name_feel(message: types.Message, state: FSMContext):
     await state.update_data(name_of_feel=message.text.lower())
     await message.answer("Choose data:")
     await Feel.wait2.set()
 
+
 async def date_feel(message: types.Message, state: FSMContext):
     await state.update_data(date_of_feel=message.text.lower())
     feel_data = await state.get_data()
     loop = asyncio.get_event_loop()
-    loop.run_in_executor(None, requests.post, "https://cernev-assistant-api.herokuapp.com/api/v1/event", {'label': feel_data['name_of_feel']},{'date': feel_data['date_of_feel']} )
-    await message.answer(f"Thank you, feel: {feel_data['name_of_feel']}, data:  {feel_data['date_of_feel']}, is recorded")
+    loop.run_in_executor(None, requests.post, "https://cernev-assistant-api.herokuapp.com/api/v1/feel",
+                         {'category': feel_data['name_of_feel'], 'date': feel_data['date_of_feel']})
+    await message.answer(
+        f"Thank you, feel: {feel_data['name_of_feel']}, data:  {feel_data['date_of_feel']}, is recorded")
     await state.finish()
+
+
+# ___________________________________BUTTON STATUS________________________________________
+@dp.callback_query_handler(lambda c: c.data == 'status')
+async def handle_action(call: types.CallbackQuery):
+    await bot.answer_callback_query(call.id)
+    await bot.send_message(call.from_user.id, 'Please,write the name of the status')
+    await Status.wait_st1.set()
+
+
+async def name_status(message: types.Message, state: FSMContext):
+    await state.update_data(name_of_status=message.text.lower())
+    await message.answer("Choose level (x/100):")
+    await Status.wait_st2.set()
+
+
+async def date_status(message: types.Message, state: FSMContext):
+    await state.update_data(date_of_status=message.text.lower())
+    status_data = await state.get_data()
+    loop = asyncio.get_event_loop()
+    loop.run_in_executor(None, requests.post, "https://cernev-assistant-api.herokuapp.com/api/v1/status",
+                         {'estimation': status_data['date_of_status'], 'label': status_data['name_of_status']})
+    await message.answer(
+        f"Thank you, status: {status_data['name_of_status']}, estimation:  {status_data['date_of_status']}, is recorded")
+    await state.finish()
+
 
 def register_handlers_food(dp: Dispatcher):
     dp.register_message_handler(start, commands="start", state="*")
     dp.register_message_handler(name_action, state=Action.state_action)
+
     dp.register_message_handler(name_feel, state=Feel.wait1)
     dp.register_message_handler(date_feel, state=Feel.wait2)
+
+    dp.register_message_handler(name_status, state=Status.wait_st1)
+    dp.register_message_handler(date_status, state=Status.wait_st2)
 
 
 if __name__ == '__main__':
