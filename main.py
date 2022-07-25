3289b6658ed840fa3d440631e6e99aafa7522f52
prometheus1547 <washi629@gmail.com>
2022-07-25 20:06:35 +0200

    start webhook instead of polling

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils.executor import start_webhook
 
 from config import TOKEN
 from handlers.main_handler import register_buttons, register_handlers
@@ -7,7 +10,36 @@ from handlers.main_handler import register_buttons, register_handlers
 bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+HEROKU_APP_NAME = 'cernev-assistant-bot'
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
+
 if __name__ == '__main__':
     register_buttons(dp)
     register_handlers(dp)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT
+    )
     executor.start_polling(dp)
