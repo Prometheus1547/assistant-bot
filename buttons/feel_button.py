5abf477bcee7adf6d4fb7d7f1c025b026ca405a2
VladislavBut <magic.needle.md@gmail.com>
2022-08-06 22:45:10 +0300

    AB-8 Удалил reply markup кнопки Feel

--- buttons/feel_button.py
+++ buttons/feel_button.py
@@ -1,16 +1,19 @@
 import asyncio
+
+import aiogram
 import requests
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
 from markups import keyboard_for_feel
 from states import States
-from config import HOST, feel_сategory
+from config import HOST
 
+feel_category = ['Energy', 'Focus', 'Health', 'Mood']
 
 async def name_feel(message: types.Message, state: FSMContext):
     await state.update_data(name_of_feel=message.text.lower())
-    if message.text in feel_сategory:
-            await message.answer("Please give the estimation:")
+    if message.text in feel_category:
+            await message.answer("Please give the estimation:", reply_markup=aiogram.types.ReplyKeyboardRemove())
             await States.wait_for_feel_estimation.set()
     else:
         await message.answer("Sorry! Wrong category! Please choose one from below:")
@@ -18,7 +21,7 @@ async def name_feel(message: types.Message, state: FSMContext):
 
 async def estimation_feel(message: types.Message, state: FSMContext):
     await state.update_data(estimation_of_feel=message.text.lower(), user_id_from_TG= message.from_user.id)
-    if message.text not in feel_сategory:
+    if message.text not in feel_category:
         feel_data = await state.get_data()
         loop = asyncio.get_event_loop()
         loop.run_in_executor(None, requests.post, f"{HOST}api/v1/feel",
--- config.py
+++ config.py
@@ -1,3 +1,2 @@
 HOST = 'https://cernev-assistant-api.herokuapp.com/'
 PORT = '8080'
-feel_сategory = ['Energy', 'Focus', 'Health', 'Mood']
