afcb20b2e41c05c4d71351410ea92c2a94abcb42
prometheus1547 <washi629@gmail.com>
2022-08-14 19:12:56 +0200

    fix host

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-HOST = 'http://cernev-assistant-api-test.eu-central-1.elasticbeanstalk.com/'
+HOST = 'http://cernev-assistant-api-prod.eu-central-1.elasticbeanstalk.com/'
 PORT = '5000'
 SLEEP_TIME = 1.5  # seconds
